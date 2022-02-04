e75ac051d345fbc4ebffb003b6f99b8509264e10
github username <github email address>
2022-02-04 02:11:43 -0500

    messages now correctly render

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import { ThemeProvider } from '@material-ui//styles';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-import { theme } from "./themes/theme";
-import Routes from "./routes";
+import { theme } from './themes/theme';
+import Routes from './routes';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <MuiThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      </MuiThemeProvider>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<BrowserRouter>
+					<Routes />
+				</BrowserRouter>
+			</ThemeProvider>
+		</Provider>
+	);
 }
 
 export default App;
