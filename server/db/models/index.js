1b3510281fdbd150e3658bd80c9f3244e2e9952b
github username <github email address>
2022-02-15 15:49:10 -0500

    DRAFT for db redsign

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,24 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const UsesrConversations = require("./UserConversations");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+// User.hasMany(Conversation);
+// Conversation.belongsTo(User, { as: "user1" });
+// Conversation.belongsTo(User, { as: "user2" });
+// Conversation.belongsTo(User)
+
+// Created a join table for the user and conversations belongsToMany relationship
+User.belongsToMany(Conversation, { through: UsesrConversations });
+Conversation.belongsToMany(User, { through: UsesrConversations });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
-  User,
-  Conversation,
-  Message
+	User,
+	Conversation,
+	Message,
+	UsesrConversations,
 };
