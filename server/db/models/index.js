3d11f0950260ddad941077d5534856cc2d9cab77
Michael <michael.muniz94@gmail.com>
2022-02-18 19:15:37 -0500

    Update index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,13 +4,6 @@ const Message = require("./message");
 const UserConversations = require("./UserConversations");
 
 // associations
-
-// User.hasMany(Conversation);
-// Conversation.belongsTo(User, { as: "user1" });
-// Conversation.belongsTo(User, { as: "user2" });
-// Conversation.belongsTo(User)
-
-// Created a join table for the user and conversations belongsToMany relationship
 User.belongsToMany(Conversation, { through: UserConversations });
 Conversation.belongsToMany(User, { through: UserConversations });
 Message.belongsTo(Conversation);
